d3e636c75f4288ac44772c2c8afb88c9bf5ab780
Martin Jul <martin@mjul.com>
2025-02-14 09:01:35 +0100

    Use the version number from the Cargo.toml file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::Command;
 
 fn main() {
     let _matches = Command::new("philips_hue_lab")
-        .version("0.1.0")
+        .version(env!("CARGO_PKG_VERSION"))
         .about("Experimental CLI tools for Philips Hue ZigBee IoT devices.")
         .get_matches();
 }
