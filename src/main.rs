f768e6de89e7c91247657da947e9bf540e57a73d
Martin Jul <martin@mjul.com>
2025-02-14 09:32:50 +0100

    Add create-key option to request key from the bridge

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,22 @@ fn main() {
                 .help("The IP address of the Hue Bridge. You can find the IP number by opening the Philips Hue app, selecting the Hue Bridge, and pressing the information icon.")
                 .num_args(1),
         )
+        .arg(
+            Arg::new("create-key")
+                .long("create-key")
+                .help("Ask the Hue Bridge to generate an application key (press the link button on the bridge to authorize this operation).")
+                .action(clap::ArgAction::SetTrue),
+        )
         .get_matches();
 
-    // You can access the provided bridge IP address using:
     if let Some(bridge_ip) = matches.get_one::<String>("bridge") {
         println!("Using Hue Bridge at: {}", bridge_ip);
+    } else {
+        println!("No Hue Bridge IP address provided.");
+    }
+
+    if *matches.get_one::<bool>("create-key").unwrap_or(&false) {
+        println!("Requesting creation of a new application key on the Hue Bridge. Make sure you have pressed the link button on the bridge!");
+        // TODO
     }
 }
