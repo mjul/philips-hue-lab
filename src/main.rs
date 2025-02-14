01fb0e872ef51aa18900170c729704d2750f78d5
Martin Jul <martin@mjul.com>
2025-02-14 10:30:01 +0100

    Change create-key to a command

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,9 @@ fn main() {
                 .help("The IP address of the Hue Bridge. You can find the IP number by opening the Philips Hue app, selecting the Hue Bridge, and pressing the information icon.")
                 .num_args(1),
         )
-        .arg(
-            Arg::new("create-key")
-                .long("create-key")
-                .help("Ask the Hue Bridge to generate an application key (press the link button on the bridge to authorize this operation).")
-                .action(clap::ArgAction::SetTrue),
+        .subcommand(
+            Command::new("create-key")
+                .about("Ask the Hue Bridge to generate an application key. Press the Link button on the bridge to authorize this operation.")
         )
         .get_matches();
 
@@ -25,7 +23,7 @@ fn main() {
         println!("No Hue Bridge IP address provided.");
     }
 
-    if *matches.get_one::<bool>("create-key").unwrap_or(&false) {
+    if let Some(_sub_matches) = matches.subcommand_matches("create-key") {
         println!("Requesting creation of a new application key on the Hue Bridge. Make sure you have pressed the link button on the bridge!");
         // TODO
     }
